11231741a5c9946ff5bacded87a51919694b9add
justine <galaxyone258@gmail.com>
2026-09-04 21:10:40 +0800

    webscrape

--- artifacts/api-server/dist/index.mjs
+++ artifacts/api-server/dist/index.mjs
@@ -28137,7 +28137,7 @@ var require_pino = __commonJS({
     function pinoBundlerAbsolutePath(p) {
       try {
         const path = __require("path");
-        const outputDir = "C:\\Users\\crisz\\Downloads\\XloreU\\artifacts\\api-server\\dist";
+        const outputDir = "C:\\Users\\Kyouya\\OneDrive\\Documents\\Xlore-U\\Uni_Course\\artifacts\\api-server\\dist";
         return path.resolve(outputDir, p.replace(/^\.\//, ""));
       } catch (e) {
         const f = new Function("p", "return new URL(p, import.meta.url).pathname");
